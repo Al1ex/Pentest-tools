ea929c7796fe335d91ef3758705c7736bb1bba83
Al1ex <38161463+Al1ex@users.noreply.github.com>
2020-12-16 09:50:54 +0800

    Add files via upload

---
+++ 中继劫持/Inveigh/Inveigh.psm1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+Inveigh is a Windows PowerShell ADIDNS/LLMNR/mDNS/NBNS spoofer/man-in-the-middle tool.
+
+.LINK
+https://github.com/Kevin-Robertson/Inveigh
+#>
+Import-Module $PWD\Inveigh.ps1
+Import-Module $PWD\Inveigh-Relay.ps1
